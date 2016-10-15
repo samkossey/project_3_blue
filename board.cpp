74edff3cbff1bd3f2129858d4d0ce89fd6bda83f
samkossey <skossey@udel.edu>
2016-10-15 23:59:09 +0000

    without argument error handling

--- board.cpp
+++ board.cpp
@@ -13,7 +13,7 @@ struct Spot{
     bool isFlagged;
     Spot(){
         isBomb = false;
-        isRevealed = true;
+        isRevealed = false;
         isFlagged = false;
         value = 0;
     };
@@ -41,12 +41,7 @@ Spot** initializeBoard(int width, int height){
     for (int i = 0; i < width; i++){
         board[i] = new Spot[height];
     }
-    for (int j = 0; j < width; j++){
-        for (int k = 0; k < height; k++){
-            cout << board[j][k].value;
-        }
-        cout << endl;
-    }
+
     return board;
 }
 
@@ -69,8 +64,10 @@ void generateBoard(Spot** &aBoard, int width, int height, int bombs, int x, int 
     for(int i = 0 ; i < width; i++){
         for (int j = 0; j < height; j++){
             if(bombsDropped < bombs){
-                aBoard[i][j].Bomb();
+                if (!(i == x && j == y)){
+                    aBoard[i][j].Bomb();
                     bombsDropped++;
+                }
             } else {
                 break;
             }
@@ -79,9 +76,15 @@ void generateBoard(Spot** &aBoard, int width, int height, int bombs, int x, int 
             break;
         }
     }
+    int first;
+    int sec;
     for(int i = 0 ; i < width; i++){
         for (int j = 0; j < height; j++){
-            swap(aBoard[i][j],aBoard[rand() % width][rand() % height]);
+            first = rand() % width;
+            sec = rand() % height;
+            if (!(i == x && j == y || first == x && sec == y)){
+                swap(aBoard[i][j],aBoard[first][sec]);
+            }
         }
     }
     
@@ -107,17 +110,35 @@ void generateBoard(Spot** &aBoard, int width, int height, int bombs, int x, int 
     
 }
 
-bool reveal(Spot** &aBoard, int x, int y, int height, int width){
-    bool result = aBoard[x][y].Click(false);
-    if (aBoard[x][y].value == 0){
+bool reveal(Spot** &aBoard, int x, int y, int height, int width, bool flag, int &spots){
+    bool result;
+    if (aBoard[x][y].isRevealed){
+        return false;
+    }
+    if (flag){
+        return aBoard[x][y].Click(true);
+    }
+    else if (aBoard[x][y].isBomb){
+        return aBoard[x][y].Click(false);
+    }
+    else if (aBoard[x][y].value != 0){
+        spots--;
+        return aBoard[x][y].Click(false);
+    } 
+    else{
+    if (aBoard[x][y].value == 0 && !(aBoard[x][y].isRevealed)){
+        result = aBoard[x][y].Click(false);
+        spots--;
         for (int i = -1; i < 2; i++){
         for (int j = -1; j < 2; j++){
-            if (!(x+i < 0 || x+i > width || y+j < 0 || y+j > height) && aBoard[x+i][y+j].isBomb){
-                if(reveal(aBoard, x+i, y+j, height, width)){
+            if (!(x+i < 0 || x+i >= width || y+j < 0 || y+j >= height) &&
+            !(i == 0 && j == 0)){
+                if(reveal(aBoard, x+i, y+j, height, width, false, spots)){
                     throw runtime_error("We done screwed up, and auto clicked a bomb :(");
                 }
             }
         }
+    }
     }
     return result;
 }
@@ -166,8 +187,62 @@ void drawBoard(Spot** &aBoard, int width, int height){
 
 
 int main(){
-    Spot** board = initializeBoard(5, 5);
-    generateBoard(board, 5, 5, 2, 0, 0);
-    drawBoard(board, 5, 5);
-    deleteBoard(board, 5);
+    int width;
+    int height;
+    int bombs;
+    int x;
+    int y;
+    bool gameOver;
+    int safeSpots;
+    bool flag;
+    cout << "Width ";
+    cin >> width;
+    cout << "Height ";
+    cin >> height;
+    safeSpots = height * width;
+    cout << "Bombs ";
+    cin >> bombs;
+    Spot** board = initializeBoard(width, height);
+    
+    while(!gameOver && safeSpots > bombs){
+        drawBoard(board, width, height);
+        cout << "x ";
+        cin >> x;
+        cout << "y ";
+        cin >> y;
+        if (safeSpots == width * height){
+            generateBoard(board, width, height, bombs, x, y);
+            reveal(board, x, y, height, width, false, safeSpots);
+            //safeSpots--;
+        }
+        else{
+            cout << "flag (0 for no, 1 for yes) ";
+            cin >> flag;
+            if (!flag && board[x][y].isFlagged){
+                cout << "You can't click a spot that has " <<
+                "been flagged. Please unflag first or click " <<
+                "a different spot" << endl;
+                continue;
+            }
+            gameOver = reveal(board, x, y, height, width, flag, safeSpots);
+            // if (!flag){
+            //     safeSpots--;
+            // }
+        }
+    }
+    if (gameOver){
+        for (int l = 0; l < width; l++){
+            for (int m = 0; m < height; m++){
+                if (board[l][m].isBomb){
+                    board[l][m].isRevealed = true;
+                }
+            }
+        }
+        drawBoard(board, width, height);
+        cout << "You lost!!!" << endl;
+    }
+    else if (safeSpots <= bombs){
+        cout << "You won!!!" << endl;
+    }
+    deleteBoard(board, width);
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,22 @@
 # Project 3 Blue
 
 # Command Line Minesweeper
+
+
+# The following are the rules and recs we have considered for the project.
+# 1)  EXP53-CPP. Do not read uninitialized memory.This is evident from the lines-15-18,40, 49, 62, 83,84 and 126 in board.cpp.
+# 2)  EXP51-CPP. Do not delete an array through a pointer of the incorrect type. There is a destructor for deinitializing the spot, which is for creating space. So instead of including a free or delete, we create a destructor.
+# 3)  ARR32-C. Ensure size arguments for variable length arrays are in a valid range. For creating the board, we have ensured an i*j matrix with proper integer bounds. It is evident from void generateBorad, line 61 in board.cpp.
+# 4)  ARR01-C. Do not apply the sizeof operator to a pointer when taking size of an arrays. We have made use of double pointer to ensure space on the board. Sizeof operator is something we did not include for this one.
+# 5)  EXP54-CPP. Do not access an object outside its lifetime. In all the operations that need an object to be created, the object has been called within the scope of the function which is the lifetime of the object.
+// # 6)  EXP34-C. Do not deference null pointers. doesn't apply
+# 7)  EXP37-C. Call functions with the correct number and type of arguments. All the functions have been called with correct number and type of arguments. 
+# 8)  EXP45-C. Do not perform assignments in selection statements.We took care of this rec in lines 67, 85,131, 166, 210 in board.cpp.
+# 9)  EXP19-C. Use braces for the body of an if, for or while statement. We have ensured that for the body of if, for and while statements, we have included proper braces.
+# 10) EXP20-C. Perform explicit tests to determine success, true, false and equality. In the function bool reveal at line 113 in board.cpp we have created sufficient conditions for checking the equality.
+# 11) EXP15-C. Do not place a semicolon on the same line as an if, for or while statement. We have taken care to not include the statement delimiter in the same line as an if for and while statements as they are not exit controlled structures.
+# 12) EXP35-C. Do not modify objects with a temporary lifetime. No objects with temporary lifetime have been modified.
+# 13) EXP40-C. Do not modify constant objects. Constant objects have been constant throughout the program.
+// # 14) EXP46-C. Do not use a bitwise operator with Boolean like operand. 
+# 15) ARR38-C. Guarantee that library functions do not form invalid pointers.
+# 16) EXP00-C. Use paranthesis for precedence of operation. In all the files it has been taken care that proper paranthesis are used operations.
